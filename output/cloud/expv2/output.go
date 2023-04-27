0a170f1549786b6bd7a2159397d8cf3572909f2d
Ivan <2103732+codebien@users.noreply.github.com>
2023-04-27 18:40:59 +0200

    cloud/expv2: Merged v1 operations

--- output/cloud/expv2/output.go
+++ output/cloud/expv2/output.go
@@ -1,85 +1,344 @@
-// Package expv2 contains a Cloud output using a Protobuf
-// binary format for encoding payloads.
 package expv2
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"strings"
 	"time"
 
-	"github.com/mstoykov/atlas"
-	"github.com/sirupsen/logrus"
 	"go.k6.io/k6/cloudapi"
+	"go.k6.io/k6/errext"
+	"go.k6.io/k6/errext/exitcodes"
+	"go.k6.io/k6/lib"
+	"go.k6.io/k6/lib/consts"
 	"go.k6.io/k6/metrics"
 	"go.k6.io/k6/output"
 	"go.k6.io/k6/output/cloud/expv2/pbcloud"
+
+	"github.com/mstoykov/atlas"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"gopkg.in/guregu/null.v3"
 )
 
-// TestName is the default Cloud test name
-const (
-	TestName = "k6 test"
-)
+// TestName is the default k6 Cloud test name
+const TestName = "k6 test"
 
 // Output sends result data to the k6 Cloud service.
 type Output struct {
 	output.SampleBuffer
 
-	config      cloudapi.Config
-	referenceID string
+	logger logrus.FieldLogger
+	opts   lib.Options
+	config cloudapi.Config
 
-	logger          logrus.FieldLogger
-	client          *MetricsClient
+	referenceID   string
+	executionPlan []lib.ExecutionStep
+	duration      int64 // in seconds
+	thresholds    map[string][]*metrics.Threshold
+
+	metricsClient *MetricsClient
+	testsClient   *cloudapi.Client
+
 	periodicFlusher *output.PeriodicFlusher
 
 	// TODO: if the metric refactor (#2905) will introduce
 	// a sequential ID for metrics
 	// then we could reuse the same strategy here
 	activeSeries map[*metrics.Metric]aggregatedSamples
+
+	testStopFunc       func(error)
+	stopSendingMetrics chan struct{}
 }
 
-// New creates a new cloud output.
-func New(logger logrus.FieldLogger, conf cloudapi.Config) (*Output, error) {
-	mc, err := NewMetricsClient(logger, conf.Host.String, conf.Token.String)
+// New creates a new Cloud output version 2.
+func New(params output.Params) (*Output, error) {
+	conf, err := cloudapi.GetConsolidatedConfig(
+		params.JSONConfig, params.Environment, params.ConfigArgument, params.ScriptOptions.External)
 	if err != nil {
 		return nil, err
 	}
+
+	if err := validateRequiredSystemTags(params.ScriptOptions.SystemTags); err != nil {
+		return nil, err
+	}
+
+	logger := params.Logger.WithFields(logrus.Fields{"output": "cloud"})
+
+	if !conf.Name.Valid || conf.Name.String == "" {
+		scriptPath := params.ScriptPath.String()
+		if scriptPath == "" {
+			// Script from stdin without a name, likely from stdin
+			return nil, errors.New("script name not set, please specify K6_CLOUD_NAME or options.ext.loadimpact.name")
+		}
+
+		conf.Name = null.StringFrom(filepath.Base(scriptPath))
+	}
+	if conf.Name.String == "-" {
+		conf.Name = null.StringFrom(TestName)
+	}
+
+	duration, testEnds := lib.GetEndOffset(params.ExecutionPlan)
+	if !testEnds {
+		return nil, errors.New("tests with unspecified duration are not allowed when outputting data to k6 cloud")
+	}
+
+	// TODO: evaluate to introduce concurrent flush
+	//
+	//if !(conf.MetricPushConcurrency.Int64 > 0) {
+	//return nil, fmt.Errorf("metrics push concurrency must be a positive number but is %d",
+	//conf.MetricPushConcurrency.Int64)
+	//}
+
+	metricsClient, err := NewMetricsClient(logger, conf.Host.String, conf.Token.String)
+	if err != nil {
+		return nil, err
+	}
+
 	return &Output{
-		config:       conf,
-		client:       mc,
-		logger:       logger.WithFields(logrus.Fields{"output": "cloudv2"}),
-		activeSeries: make(map[*metrics.Metric]aggregatedSamples),
+		logger: logger,
+		config: conf,
+		opts:   params.ScriptOptions,
+		testsClient: cloudapi.NewClient(
+			logger, conf.Token.String, conf.Host.String, consts.Version, conf.Timeout.TimeDuration()),
+		metricsClient:      metricsClient,
+		executionPlan:      params.ExecutionPlan,
+		duration:           int64(duration / time.Second),
+		activeSeries:       make(map[*metrics.Metric]aggregatedSamples),
+		stopSendingMetrics: make(chan struct{}),
 	}, nil
 }
 
-// Start starts the output.
-func (o *Output) Start() error {
-	o.logger.Debug("Starting...")
+// validateRequiredSystemTags checks if all required tags are present.
+func validateRequiredSystemTags(scriptTags *metrics.SystemTagSet) error {
+	missingRequiredTags := []string{}
+	requiredTags := metrics.SystemTagSet(metrics.TagName |
+		metrics.TagMethod |
+		metrics.TagStatus |
+		metrics.TagError |
+		metrics.TagCheck |
+		metrics.TagGroup)
+	for _, tag := range metrics.SystemTagValues() {
+		if requiredTags.Has(tag) && !scriptTags.Has(tag) {
+			missingRequiredTags = append(missingRequiredTags, tag.String())
+		}
+	}
+	if len(missingRequiredTags) > 0 {
+		return fmt.Errorf(
+			"the cloud output needs the following system tags enabled: %s",
+			strings.Join(missingRequiredTags, ", "),
+		)
+	}
+	return nil
+}
 
-	// TODO: merge here the part executed by v1 when we will drop it
+// Start calls the k6 Cloud API to initialize the test run, and then starts the
+// goroutine that would listen for metric samples and send them to the cloud.
+func (out *Output) Start() error {
+	if out.config.PushRefID.Valid {
+		out.referenceID = out.config.PushRefID.String
+		out.logger.WithField("referenceId", out.referenceID).Debug("directly pushing metrics without init")
+		out.startBackgroundProcesses()
+		return nil
+	}
+
+	thresholds := make(map[string][]string)
+
+	for name, t := range out.thresholds {
+		for _, threshold := range t {
+			thresholds[name] = append(thresholds[name], threshold.Source)
+		}
+	}
+	maxVUs := lib.GetMaxPossibleVUs(out.executionPlan)
+
+	testRun := &cloudapi.TestRun{
+		Name:       out.config.Name.String,
+		ProjectID:  out.config.ProjectID.Int64,
+		VUsMax:     int64(maxVUs),
+		Thresholds: thresholds,
+		Duration:   out.duration,
+	}
+
+	response, err := out.testsClient.CreateTestRun(testRun)
+	if err != nil {
+		return err
+	}
+	out.referenceID = response.ReferenceID
+
+	if response.ConfigOverride != nil {
+		out.logger.WithFields(logrus.Fields{
+			"override": response.ConfigOverride,
+		}).Debug("overriding config options")
+		out.config = out.config.Apply(*response.ConfigOverride)
+	}
+
+	out.startBackgroundProcesses()
+
+	out.logger.WithFields(logrus.Fields{
+		"name":        out.config.Name,
+		"projectId":   out.config.ProjectID,
+		"duration":    out.duration,
+		"referenceId": out.referenceID,
+	}).Debug("Started!")
+	return nil
+}
+
+func (o *Output) startBackgroundProcesses() error {
+	// TODO: Implement a new time series based cloud aggregation
+	// https://github.com/grafana/k6/issues/1700
+
 	pf, err := output.NewPeriodicFlusher(o.config.MetricPushInterval.TimeDuration(), o.flushMetrics)
 	if err != nil {
 		return err
 	}
-	o.logger.Debug("Started!")
+
 	o.periodicFlusher = pf
 	return nil
 }
 
-// StopWithTestError stops the output.
+// Stop gracefully stops all metric emission from the output: when all metric
+// samples are emitted, it makes a cloud API call to finish the test run.
+//
+// Deprecated: use StopWithTestError() instead.
+func (out *Output) Stop() error {
+	return out.StopWithTestError(nil)
+}
+
+// StopWithTestError gracefully stops all metric emission from the output: when
+// all metric samples are emitted, it makes a cloud API call to finish the test
+// run. If testErr was specified, it extracts the RunStatus from it.
 func (o *Output) StopWithTestError(testErr error) error {
-	o.logger.Debug("Stopping...")
-	defer o.logger.Debug("Stopped!")
+	o.logger.Debug("Stopping the cloud output...")
 	o.periodicFlusher.Stop()
-	return nil
+
+	o.logger.Debug("Metric emission stopped, calling cloud API...")
+	err := o.testFinished(testErr)
+	if err != nil {
+		o.logger.WithFields(logrus.Fields{"error": err}).Warn("Failed to send test finished to the cloud")
+	} else {
+		o.logger.Debug("Cloud output successfully stopped!")
+	}
+	return err
 }
 
-// SetReferenceID sets the Cloud's test ID.
-func (o *Output) SetReferenceID(refID string) {
-	o.referenceID = refID
+// Description returns the URL with the test run results.
+func (out *Output) Description() string {
+	return fmt.Sprintf("cloud (%s)", cloudapi.URLForResults(out.referenceID, out.config))
 }
 
-// AddMetricSamples receives a set of metric samples.
+// getRunStatus determines the run status of the test based on the error.
+func (out *Output) getRunStatus(testErr error) cloudapi.RunStatus {
+	if testErr == nil {
+		return cloudapi.RunStatusFinished
+	}
+
+	var err errext.HasAbortReason
+	if errors.As(testErr, &err) {
+		abortReason := err.AbortReason()
+		switch abortReason {
+		case errext.AbortedByUser:
+			return cloudapi.RunStatusAbortedUser
+		case errext.AbortedByThreshold:
+			return cloudapi.RunStatusAbortedThreshold
+		case errext.AbortedByScriptError:
+			return cloudapi.RunStatusAbortedScriptError
+		case errext.AbortedByScriptAbort:
+			return cloudapi.RunStatusAbortedUser // TODO: have a better value than this?
+		case errext.AbortedByTimeout:
+			return cloudapi.RunStatusAbortedLimit
+		case errext.AbortedByOutput:
+			return cloudapi.RunStatusAbortedSystem
+		case errext.AbortedByThresholdsAfterTestEnd:
+			// The test run finished normally, it wasn't prematurely aborted by
+			// anything while running, but the thresholds failed at the end and
+			// k6 will return an error and a non-zero exit code to the user.
+			//
+			// However, failures are tracked somewhat differently by the k6
+			// cloud compared to k6 OSS. It doesn't have a single pass/fail
+			// variable with multiple failure states, like k6's exit codes.
+			// Instead, it has two variables, result_status and run_status.
+			//
+			// The status of the thresholds is tracked by the binary
+			// result_status variable, which signifies whether the thresholds
+			// passed or failed (failure also called "tainted" in some places of
+			// the API here). The run_status signifies whether the test run
+			// finished normally and has a few fixed failures values.
+			//
+			// So, this specific k6 error will be communicated to the cloud only
+			// via result_status, while the run_status will appear normal.
+			return cloudapi.RunStatusFinished
+		}
+	}
+
+	// By default, the catch-all error is "aborted by system", but let's log that
+	out.logger.WithError(testErr).Debug("unknown test error classified as 'aborted by system'")
+	return cloudapi.RunStatusAbortedSystem
+}
+
+// SetThresholds receives the thresholds before the output is Start()-ed.
+func (out *Output) SetThresholds(scriptThresholds map[string]metrics.Thresholds) {
+	thresholds := make(map[string][]*metrics.Threshold)
+	for name, t := range scriptThresholds {
+		thresholds[name] = append(thresholds[name], t.Thresholds...)
+	}
+	out.thresholds = thresholds
+}
+
+// SetTestRunStopCallback receives the function that stops the engine on error
+func (out *Output) SetTestRunStopCallback(stopFunc func(error)) {
+	out.testStopFunc = stopFunc
+}
+
+func (out *Output) shouldStopSendingMetrics(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	if errResp, ok := err.(cloudapi.ErrorResponse); ok && errResp.Response != nil {
+		return errResp.Response.StatusCode == http.StatusForbidden && errResp.Code == 4
+	}
+
+	return false
+}
+
+func (out *Output) testFinished(testErr error) error {
+	// TODO: why do we need to check the config also?
+	if out.referenceID == "" || out.config.PushRefID.Valid {
+		return nil
+	}
+
+	testTainted := false
+	thresholdResults := make(cloudapi.ThresholdResult)
+	for name, thresholds := range out.thresholds {
+		thresholdResults[name] = make(map[string]bool)
+		for _, t := range thresholds {
+			thresholdResults[name][t.Source] = t.LastFailed
+			if t.LastFailed {
+				testTainted = true
+			}
+		}
+	}
+
+	runStatus := out.getRunStatus(testErr)
+	out.logger.WithFields(logrus.Fields{
+		"ref":        out.referenceID,
+		"tainted":    testTainted,
+		"run_status": runStatus,
+	}).Debug("Sending test finished")
+
+	return out.testsClient.TestFinished(out.referenceID, thresholdResults, testTainted, runStatus)
+}
+
 func (o *Output) flushMetrics() {
+	select {
+	case <-o.stopSendingMetrics:
+		return
+	default:
+	}
+
 	if o.referenceID == "" {
 		// TODO: should it warn?
 		return
@@ -109,10 +368,21 @@ func (o *Output) flushMetrics() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), o.config.MetricPushInterval.TimeDuration())
 	defer cancel()
-
-	err := o.client.Push(ctx, o.referenceID, &pbcloud.MetricSet{Metrics: metricSet})
+	err := o.metricsClient.Push(ctx, o.referenceID, &pbcloud.MetricSet{Metrics: metricSet})
 	if err != nil {
-		o.logger.WithError(err).Error("failed to push metrics to the cloud")
+		o.logger.WithError(err).Error("Failed to push metrics to the cloud")
+
+		if o.shouldStopSendingMetrics(err) {
+			o.logger.WithError(err).Warn("Interrupt sending metrics to cloud due to an error")
+			serr := errext.WithAbortReasonIfNone(
+				errext.WithExitCodeIfNone(err, exitcodes.ExternalAbort),
+				errext.AbortedByOutput,
+			)
+			if o.config.StopOnError.Bool {
+				o.testStopFunc(serr)
+			}
+			close(o.stopSendingMetrics)
+		}
 		return
 	}
 
@@ -198,7 +468,6 @@ func (as *aggregatedSamples) MapAsProto(refID string) []*pbcloud.TimeSeries {
 			// skip trend metrics
 			continue
 		}
-
 		pb := pbcloud.TimeSeries{}
 		// TODO: optimize removing Map
 		// and using https://github.com/grafana/k6/issues/2764
